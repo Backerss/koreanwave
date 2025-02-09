b7ee07f8e180fefc8e9dcb388942cbc7319de2a4
Backer <AsanRodnuan2546@gmail.com>
2025-02-09 13:24:37 +0700

    chore: Update generation time in koreanwave.sql

--- data/koreanwave.sql
+++ data/koreanwave.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 08, 2025 at 07:29 PM
+-- Generation Time: Feb 09, 2025 at 06:07 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
